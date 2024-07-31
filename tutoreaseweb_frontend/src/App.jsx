b196c881e171266bb4eee220f9377008c4816d25
Bukhobenkosi <mbindabukhobenkosi@gmail.com>
2024-07-31 14:55:37 +0200

    Student and Tutor registation pages

--- tutoreaseweb_frontend/src/App.jsx
+++ tutoreaseweb_frontend/src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import ClassSession from "./components/ClassSession.jsx";
 import NavBar from "./navigation/NavBar.jsx";
 import TutorProfile from "./components/TutorProfile.jsx";
-
+import StudentRegistration from "./components/StudentRegistration.jsx";
+import TutorRegistration from "./components/TutorRegistration.jsx";
 
 function App() {
-
-  return (
-
-      <div>
-          <NavBar />
-          <Routes>
-              <Route exact path="/" element={<TutorProfile />} />
-              <Route path="/class_session" element={<ClassSession />} />
-              <Route path="/tutor_profile" element={<TutorProfile/>}/>
-              </Routes>
-      </div>
-
-
-  )
+    return (
+        <div>
+            <NavBar />
+            <Routes>
+                <Route exact path="/" element={<TutorProfile />} />
+                <Route path="/class_session" element={<ClassSession />} />
+                <Route path="/tutor_profile" element={<TutorProfile />} />
+                <Route path="/student_registration" element={<StudentRegistration />} />
+                <Route path="/tutor_registration" element={<TutorRegistration />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
