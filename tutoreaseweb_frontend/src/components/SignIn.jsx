646c053a4e3cad97750eec71c3675c134bc57d1e
Thandolwethu Zamasiba Khoza <221797289@mycput.ac.za>
2024-08-05 13:40:24 +0200

    Please check the added files and link them with the entire project. I created their css separetly to avoid problems

---
+++ tutoreaseweb_frontend/src/components/SignIn.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Landing_Signin.css';
+
+function SignIn() {
+    return (
+        <div className="SignIn">
+            <div className="SignIn-main">
+                <header className="SignIn-header">
+                    <img src={`public/logo.png`} className="SignIn-logo" alt="logo" />
+                </header>
+                <form className="SignIn-form">
+                    <input type="text" placeholder="Username" className="SignIn-input" />
+                    <input type="password" placeholder="Password" className="SignIn-input" />
+                    <Link to="/forgot-password" className="Forgot-password-link">Forgot password?</Link>
+                    <button type="submit" className="SignIn-button">Sign In</button>
+                </form>
+                <Link to="/" className="Join-us-link">Join us</Link>
+            </div>
+        </div>
+    );
+}
+
+export default SignIn;
